dfb2e7a57a4f22cf47e77163ae55dcdd097acd6f
Javier Elohim Burgos Chaguezac <84932679+JavierBurgos-web@users.noreply.github.com>
2024-05-16 10:51:16 -0500

    Update a_exploracion.py

--- 3_notebook/a_exploracion.py
+++ 3_notebook/a_exploracion.py
@@ -5,5 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Cargando datos
-expuestos = pd.read_text('https://raw.githubusercontent.com/JavierBurgos-web/Alll_Proyecto_Finanzas/main/2_data/BD_Expuestos.txt')
-sociodemograficos = pd_read_text('https://raw.githubusercontent.com/JavierBurgos-web/Alll_Proyecto_Finanzas/main/2_data/BD_Sociodemograficas.txt')
+expuestos = pd.read_csv('https://raw.githubusercontent.com/JavierBurgos-web/Alll_Proyecto_Finanzas/main/2_data/BD_Expuestos.txt', sep='\t')
+sociodemograficos = pd.read_csv('https://raw.githubusercontent.com/JavierBurgos-web/Alll_Proyecto_Finanzas/main/2_data/BD_Sociodemograficas.txt', sep='\t')
+siniestros = pd.read_csv('https://media.githubusercontent.com/media/JavierBurgos-web/Alll_Proyecto_Finanzas/main/2_data/DB_Siniestros.txt', sep=',')
